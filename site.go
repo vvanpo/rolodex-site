cf6b6c272be1db75b131a5740c6ec62d3afcb247
Victor van Poppelen <vvanpo@gmail.com>
2014-11-28 18:31:02 -0500

    template.html:  Bootstrap starter template

--- site.go
+++ site.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
+var templates = template.Must(template.ParseFiles("template.html"))
+
+type page struct {
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World\n%s", r.URL.Path[1:])
+	p := page{}
+	templates.Execute(w, p)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public/")))
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
